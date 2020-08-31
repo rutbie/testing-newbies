56994a7931d19c6681454d932d374521b0a118d6
Ruta Bielyte <ruta.bielyte@devbridge.com>
2020-08-31 17:55:27 +0300

    Add non-UI login test

--- cypress/integration/homePage.spec.js
+++ cypress/integration/homePage.spec.js
@@ -5,12 +5,10 @@ const homePage = new HomePage();
 const loginPage = new LoginPage();
 
 describe('Home Page', () => {
-    beforeEach(() => {
+    it('should be able to log in via UI', () => {
         cy.setCookie('cookieconsent_status', 'dismiss');
         cy.setCookie('welcomebanner_status', 'dismiss');
-    });
 
-    it('should be able to log in via UI', () => {
         homePage.open();
         homePage.clickAccountNavigationButton();
         homePage.clickLogInNavigationButton();
@@ -21,6 +19,28 @@ describe('Home Page', () => {
         loginPage.assertLogInButtonIsEnabled(true);
         loginPage.clickLogInSubmitButton();
 
+        homePage.clickAccountNavigationButton();
+        homePage.assertEmail(Cypress.env('email'));
+    });
+
+    it('should be able to log in without UI', () => {
+        const body = {
+            email: Cypress.env('email'),
+            password: Cypress.env('password')
+        }
+
+        cy.request({
+            method: 'POST',
+            url: '/rest/user/login',
+            body,
+        }).then((response) => {
+            window.localStorage.setItem('token', response.body.authentication.token);
+            cy.setCookie('token', response.body.authentication.token);
+            cy.setCookie('cookieconsent_status', 'dismiss');
+            cy.setCookie('welcomebanner_status', 'dismiss');
+        });
+
+        cy.visit('/');
         homePage.clickAccountNavigationButton();
         homePage.assertEmail(Cypress.env('email'));
     });
